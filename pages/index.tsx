8e203aaad77ec651eee4c44460ccca9d914d2c88
j1hwan <jihwan@pm.me>
2023-12-17 21:27:51 +0900

    feat: 메인 페이지 세팅

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
 import { NextPage } from 'next';
+import { Box, Heading, Flex } from '@chakra-ui/react';
 import { ServiceLayout } from '@/component/service_layout';
 
 const IndexPage: NextPage = function () {
-  return <ServiceLayout title="Home">test</ServiceLayout>;
+  return (
+    <ServiceLayout title="Home">
+      <Box maxW="md" mx="auto">
+        <img src="/main_logo.svg" alt="메인 로고" />
+        <Flex justify="center">
+          <Heading>#BlahBlah</Heading>
+        </Flex>
+      </Box>
+      <Box>로그인 버튼</Box>
+    </ServiceLayout>
+  );
 };
 
 export default IndexPage;
