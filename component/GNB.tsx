da39dd989a881b64f32b7a09fff600d43db46235
j1hwan <jihwan@pm.me>
2023-12-20 15:25:37 +0900

    feat: GNB - 로그인/로그아웃 버튼기능 활성화

--- component/GNB.tsx
+++ component/GNB.tsx
@@ -2,7 +2,21 @@ import { Box, Button, Flex, Spacer } from '@chakra-ui/react';
 import { useAuth } from '@/contexts/auth_user_context';
 
 const GNB = function () {
-  const { loading, authUser, signOut } = useAuth();
+  const { loading, authUser, signOut, signInWithGoogle } = useAuth();
+  const authInitialized = loading || authUser === null;
+  const logInBtn = (
+    <Button
+      fontSize="sm"
+      fontWeight="600"
+      color="white"
+      bg="pink.400"
+      _hover={{ background: 'pink.300' }}
+      onClick={signInWithGoogle}
+    >
+      로그인
+    </Button>
+  );
+  const logOutBtn = <Button onClick={signOut}>로그아웃</Button>;
   return (
     <Box>
       <Flex minH="60px" py={{ base: 2 }} px={{ base: 4 }} align="center" maxW="md" mx="auto">
@@ -10,9 +24,7 @@ const GNB = function () {
         <Box flex="1">
           <img style={{ height: '40px' }} src="/logo.svg" alt="logo" />
         </Box>
-        <Box justifyContent="flex-end">
-          <Button>로그인</Button>
-        </Box>
+        <Box justifyContent="flex-end">{authInitialized ? logInBtn : logOutBtn}</Box>
       </Flex>
     </Box>
   );
