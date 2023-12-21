fdbb2562018e1656a1f12c91026ce169052d12e7
j1hwan <jihwan@pm.me>
2023-12-21 17:29:37 +0900

    feat: 사용자 정보 서버사이드 저장

---
+++ pages/api/member.add.ts
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from 'next';
+import FirebaseAdmin from '@/models/firebase_admin';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // InAuthUser 참고
+  const { uid, email, displayName, photoURL } = req.body;
+  if (uid === undefined || uid === null) {
+    return res.status(400).json({ result: false, message: 'uid is required' });
+  }
+  try {
+    const addResult = await FirebaseAdmin.getInstance()
+      .Firebase.collection('members')
+      .add({
+        uid,
+        email: email ?? '',
+        displayName: displayName ?? '',
+        photoURL: photoURL ?? '',
+      });
+    return res.status(200).json({ result: true, id: addResult });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ result: false });
+  }
+}
