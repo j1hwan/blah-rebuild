0b14fb443f1013b9cb04e5e9d00e87fda4e347f1
j1hwan <jihwan@pm.me>
2023-12-23 12:15:11 +0900

    feat: 공통 에러 핸들링

---
+++ controllers/errror/handle_error.ts
@@ -0,0 +1,17 @@
+import { NextApiResponse } from 'next';
+import { serialize } from 'v8';
+import CustomServerError from './custom_server_error';
+
+const handleError = (err: unknown, res: NextApiResponse) => {
+  let unknownErr = err;
+  if (err instanceof CustomServerError === false) {
+    unknownErr = new CustomServerError({ statusCode: 499, message: 'Unknown Error' });
+  }
+  const customError = unknownErr as CustomServerError;
+  res
+    .status(customError.statusCode)
+    .setHeader('location', customError.location ?? '')
+    .send(customError.serializeError());
+};
+
+export default handleError;
--- pages/api/member.add.ts
+++ pages/api/member.add.ts
@@ -1,7 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from 'next';
-import MemberModel from '@/models/member/member.model';
 import MemberCtrl from '@/controllers/member.ctrl';
+import handleError from '@/controllers/errror/handle_error';
+import CustomServerError from '@/controllers/errror/custom_server_error';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method } = req;
@@ -9,10 +10,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   try {
     if (supportedMethods.indexOf(method!) === -1) {
       // 에러반환
+      throw new CustomServerError({ statusCode: 400, message: '지원하지 않는 메소드' });
     }
     await MemberCtrl.add(req, res);
   } catch (err) {
     console.error(err);
     // 에러처리
+    handleError(err, res);
   }
 }
