03ba50d7fa49136fc12da699c5f6b16da321af80
j1hwan <jihwan@pm.me>
2023-12-28 17:30:07 +0900

    feat: 메시지 등록 API

---
+++ controllers/message.ctrl.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import BadReqError from './error/bad_request_error';
+import messageModel from '@/models/message/message.model';
+
+async function post(req: NextApiRequest, res: NextApiResponse) {
+  const { uid, message, author } = req.body;
+  if (uid === undefined) {
+    throw new BadReqError('uid가 누락되었습니다.');
+  }
+  if (message === undefined) {
+    throw new BadReqError('message가 누락되었습니다.');
+  }
+  await messageModel.post({ uid, message, author });
+  return res.status(200).end();
+}
+
+const MessageCtrl = {
+  post,
+};
+
+export default MessageCtrl;
---
+++ models/message/message.model.ts
@@ -0,0 +1,46 @@
+import { firestore } from 'firebase-admin';
+import CustomServerError from '@/controllers/error/custom_server_error';
+import FirebaseAdmin from '../firebase_admin';
+
+const MEMEBER_COL = 'members';
+const MESSAGE_COL = 'messages';
+const SCR_NAME_COL = 'screen_names';
+
+const { Firestore } = FirebaseAdmin.getInstance();
+
+async function post({
+  uid,
+  message,
+  author,
+}: {
+  uid: string;
+  message: string;
+  author?: { displayName: string; phoroURL?: string };
+}) {
+  const memberRef = Firestore.collection(MEMEBER_COL).doc(uid);
+  await Firestore.runTransaction(async (transaction) => {
+    const memberDoc = await transaction.get(memberRef);
+    if (memberDoc.exists === false) {
+      throw new CustomServerError({ statusCode: 400, message: 'Member not found' });
+    }
+    const newMessageRef = memberRef.collection(MESSAGE_COL).doc();
+    const newMessageBody: {
+      message: string;
+      createAt: firestore.FieldValue;
+      author?: { displayName: string; photoURL?: string };
+    } = {
+      message,
+      createAt: firestore.FieldValue.serverTimestamp(),
+    };
+    if (author !== undefined) {
+      newMessageBody.author = author;
+    }
+    await transaction.set(newMessageRef, newMessageBody);
+  });
+}
+
+const messageModel = {
+  post,
+};
+
+export default messageModel;
