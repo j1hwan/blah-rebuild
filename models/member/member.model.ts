e6150c670d6cfc8e212acd403ddb29e90ed81f8d
j1hwan <jihwan@pm.me>
2023-12-28 16:31:43 +0900

    chore: change typo Firebase => Firestore

--- models/member/member.model.ts
+++ models/member/member.model.ts
@@ -9,9 +9,9 @@ type AddResult = { result: true; id: string } | { result: false; message: string
 async function add({ uid, displayName, email, photoURL }: InAuthUser): Promise<AddResult> {
   try {
     const screenName = (email as string).replace('@gmail.com', '');
-    const addResult = await FirebaseAdmin.getInstance().Firebase.runTransaction(async (transaction) => {
-      const memeberRef = FirebaseAdmin.getInstance().Firebase.collection(MEMEBER_COL).doc(uid);
-      const screenNameRef = FirebaseAdmin.getInstance().Firebase.collection(SCR_NAME_COL).doc(screenName);
+    const addResult = await FirebaseAdmin.getInstance().Firestore.runTransaction(async (transaction) => {
+      const memeberRef = FirebaseAdmin.getInstance().Firestore.collection(MEMEBER_COL).doc(uid);
+      const screenNameRef = FirebaseAdmin.getInstance().Firestore.collection(SCR_NAME_COL).doc(screenName);
       // memeberRef를 이용하여 등록된 사용자 정보에 접근
       const memberDoc = await transaction.get(memeberRef);
       if (memberDoc.exists) {
@@ -38,7 +38,7 @@ async function add({ uid, displayName, email, photoURL }: InAuthUser): Promise<A
 }
 
 async function findByScreenName(ScreenName: stirng): Promise<InAuthUser | null> {
-  const memberRef = FirebaseAdmin.getInstance().Firebase.collection(SCR_NAME_COL).doc(ScreenName);
+  const memberRef = FirebaseAdmin.getInstance().Firestore.collection(SCR_NAME_COL).doc(ScreenName);
   const memberDoc = await memberRef.get();
   if (memberDoc.exists === false) {
     return null;
