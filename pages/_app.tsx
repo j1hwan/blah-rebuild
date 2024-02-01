f109b7d7cd0b2d0c0379b81fbde8a3f29cac6d1f
jiihwan <jihwan@pm.me>
2024-02-01 20:40:19 +0900

    chore: ESLint import/order 규칙 준수를 위한 import 순서 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import { AuthUserProvider } from '@/contexts/auth_user_context';
 import '../styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps /*, AppContext */ } from 'next/app';
+import { AuthUserProvider } from '@/contexts/auth_user_context';
 
 const MyApp = function ({ Component, pageProps }: AppProps) {
   return (
